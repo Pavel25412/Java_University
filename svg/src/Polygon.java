2bb9cc02f2f5ed85141d422b862c0dfa6c770aa6
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-03-17 15:48:41 +0100

    obliczanie BoundingBox dla wielokątu

--- svg/src/Polygon.java
+++ svg/src/Polygon.java
@@ -3,6 +3,25 @@ import java.util.Arrays;
 public class Polygon {
     private Point[] points;
 
+    public BoundingBox boundingBox() {
+        Point p0 = new Point(points[0]); // lewy górny róg
+        Point p1 = new Point(points[0]); // prawy dolny róg
+        for (Point p : points) {
+            if (p.getX() < p0.getX()) p0.setX(p.getX());
+            if (p.getX() > p1.getX()) p1.setX(p.getX());
+            if (p.getY() < p0.getY()) p0.setY(p.getY());
+            if (p.getY() > p1.getY()) p1.setY(p.getY());
+        }
+
+        return new BoundingBox(
+                p0.getX(),
+                p0.getY(),
+                p1.getX() - p0.getX(),
+                p1.getY() - p0.getY()
+        );
+
+    }
+
     public Polygon(Point[] points) {
         this.points = new Point[points.length];
         for (int i = 0; i < points.length; i++) {
