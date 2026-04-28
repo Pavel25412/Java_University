e1385ff9b0c45f6b3a8f8d3a0019d40eaf04f222
Blu <304entity304@gmail.com>
2026-04-28 16:02:25 +0200

    initial

---
+++ generic/src/CustomList.java
@@ -0,0 +1,79 @@
+public class CustomList<T> {
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node<T>head;
+    private Node<T> tail;
+
+    public CustomList() {
+        this.head = this.tail = null;
+    }
+
+    public void addLast(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (tail == null) {
+            this.tail = this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    public T getLast() {
+        if (tail == null) {
+            throw new RuntimeException("Lista jest pusta");
+        }
+        return tail.value;
+    }
+
+    public void addFirst (T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null){
+            this.head = this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+
+    public T getFirst() {
+        if (head == null) {
+            throw new RuntimeException("Lista jest pusta");
+        }
+        return head.value;
+    }
+
+    public T removeFirst () {
+        if (head == null) {
+            throw new RuntimeException("Lista jest pusta");
+        }
+        T value = head.value;
+        this.head = this.head.next;
+
+        if (this.head == null) {
+            this.tail = null; // lista stała się pustą
+        }
+        return value;
+    }
+
+    public T removeLast () {
+        if (tail == null) {
+            throw new RuntimeException("Lista jest pusta");
+        }
+        T value = tail.value;
+        Node<T> current = this.head;
+        while (current.next != tail) {
+            current = current.next;
+        }
+        tail = current;
+        tail.next = null;
+        return value;
+    }
+}
---
+++ generic/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[] args) {
+        CustomList<Integer> customList = new CustomList<>();
+        customList.addLast(0);
+        customList.addLast(-1);
+        customList.addLast(5);
+        customList.addLast(-3);
+        customList.addFirst(100);
+        customList.addLast(33);
+
+        System.out.println(customList.getFirst());
+        System.out.println(customList.getLast());
+    }
+}
